6cf482c11d0b402fb730c4c4684a67e52b8b2c83
uglycoal <41569944+uglycoal@users.noreply.github.com>
2020-04-06 15:50:53 +0300

    не работает ахахахахахахахаа

--- src/TimedPost.hx
+++ src/TimedPost.hx
@@ -28,7 +28,6 @@ class TimedPost
         var timer:haxe.Timer = new haxe.Timer(10000);
         timer.run = function() 
         {
-            var r = 0;
             trace("timerdone");
             var page = Std.random(100);
             trace(page);
@@ -37,32 +36,84 @@ class TimedPost
             rget.onData = function (data:String)
             {
                 var jlist:Array<Dynamic> = Json.parse(data);
-                r = Math.ceil(Std.random(jlist.length));
-                var choose = jlist[r];
-                var taglist:Array<String> = choose.tags;
-                /*var blacklist:Array<String> = [];
-                for (tag in taglist)
-                {
-                    var result:Int = blacklist.indexOf(tag);
-                    switch(result)
-                    {
-                        case -1: trace("good");
-                        default: trace("ah! this is not good. should reroll");
-                    }
-                } */
-                if (choose != null) 
-                { 
-                    trace(choose.file_url);
-                        
-                    var nm = StringTools.replace(choose.file_url, "https://r34-json-api.herokuapp.com/images?url=","");
-                    var xx:MessageCreate = {
-                        content: nm
-                    };
-                    var ss = new Endpoints(bot);
-                    ss.sendMessage("696397752611111012", xx);
+                var blacklist:Array<String> = R34.blackList;
+                var r = Math.ceil(Std.random(jlist.length));
+                var choose:RFile = jlist[r];
+                trace("i am here");
+                if (choose != null) { 
+                    var taglist:Array<String> = choose.tags;
+                
+                    var finding = true;
+                    while (finding == true)  {
+                        trace("i have come");
+                        var fail = false;
+                        for (tag in taglist)
+                        {   
+                            var result:Int = blacklist.indexOf(tag);
+                            if (result >= 0) {
+                                fail = true;
+                                break;
+                            }
+                        } 
+
+                        if (!fail) {
+                            finding = false;
+                            var nm = StringTools.replace(choose.file_url, "https://r34-json-api.herokuapp.com/images?url=","");
+                            trace("bitch");
+                            sendMessage(nm, "696397752611111012");
+                        }
+                        else {
+                            if (++r < jlist.length) {
+                                choose = jlist[r];
+                                taglist = choose.tags;
+                            } else {
+                                finding = false;
+                                sendMessage("мне нельзя такое показывать", "696397752611111012");
+                            }
+                        }
+                    }         
+                } else {
+                    sendMessage("ничего не нашел", "696397752611111012");
                 }
             }
-            rget.request();
         }           
-    } 
+    }
+    public static function sendMessage(text:String, channleId:String) 
+    {
+        var msg:MessageCreate = {
+            content: text
+        };
+        var end = new Endpoints(bot);
+        end.sendMessage(channleId, msg);
+    }
+}
+
+
+
+typedef RFile = {
+var score:String;
+var file_url:String;
+var tags:Array<String>;
+}
+class R34 
+{
+    public static var blackList:Array<String> = [
+        "futanari",
+        "big_belly",
+        "rimming",
+        "hyper",
+        "inflation",
+        "lactating",
+        "male/male",
+        "male_on_feral",
+        "pooping",
+        "score:0",
+        "urine",
+        "belly_hair",
+        "chest_hair",
+        "slightly_chubby",
+        "overweight",
+        "big_muscles",
+        "human_penetrating_feral"
+    ];
 }
