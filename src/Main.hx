55508f7d8a7a03db19ec7e4eea45e23e5bcebcde
fataliti <fataliti22@gmail.com>
2020-04-08 14:27:14 +0600

    g ga info

--- src/Main.hx
+++ src/Main.hx
@@ -1,21 +1,18 @@
-
-
-
-import sys.FileSystem;
-import sys.io.File;
+import com.raidandfade.haxicord.types.structs.Status;
+import com.raidandfade.haxicord.types.structs.Embed;
 import com.raidandfade.haxicord.endpoints.Typedefs.MessageCreate;
 import com.raidandfade.haxicord.endpoints.Endpoints;
+import com.raidandfade.haxicord.types.Message;
+import com.raidandfade.haxicord.DiscordClient;
+
 
 import haxe.Http;
 import haxe.Json;
 import haxe.Timer;
-
-
-import com.raidandfade.haxicord.types.Message;
-import com.raidandfade.haxicord.DiscordClient;
-
 import haxe.rtti.Meta;
 
+import sys.FileSystem;
+import sys.io.File;
 
 
 class Main {
@@ -24,6 +21,8 @@ class Main {
     static var token:String = "NjY2Mjk5MTM0NTc5NDQxNjY0.Xm3FkQ.qgW0_zAC3yrjBBEerfd3KFo3Vmo"; 
     static var commandList:Array<String> = [];
 
+    static var rssLink:String;
+    static var rssPage:Int;
     
     static function main() {
         bot = new DiscordClient(token);
@@ -32,13 +31,23 @@ class Main {
         bot.ws.onClose = onClose;
 
         if (FileSystem.exists("mlist.txt")) {
-            var t = File.getContent("mlist.txt");
-            var j:Array<String> = Json.parse(t);
-            for(tag in j) {
+            var mt = File.getContent("mlist.txt");
+            var mj:Array<String> = Json.parse(mt);
+            for(tag in mj) {
                 if (R34.blackList.indexOf(tag) == -1) {
                     R34.blackList.push(tag);
                 }
             }
+        }
+
+        if (FileSystem.exists("rss.txt")) {
+            var rt = File.getContent("rss.txt");
+            var rj:RSSFile = Json.parse(rt);
+            rssLink = rj.tags;
+            rssPage = rj.page;
+        } else {
+            rssLink = "https://gelbooru-xsd8bjco8ukx.runkit.sh/posts?tags=score:20+1girl&page=";
+            rssPage = 200;
         }
 
         var statics = Meta.getStatics(Main);
@@ -49,6 +58,103 @@ class Main {
         trace("all ready");
     }
 
+    public static function onReady() {
+        trace('invite link: ${bot.getInviteLink()}');
+
+        var a:Activity = {
+            name: "!info",
+            type: 0,
+        };
+        var s:Status = {
+            game: a,
+            afk: false,
+            status: "online",
+        };
+
+        bot.setStatus(s);
+
+        var timer:Timer = new Timer(60 * 1000 * 10);
+        timer.run = function() 
+        {   
+            var animeChan = "638490021678284820";
+            var page = Std.random(rssPage);
+            var find = rssLink+Std.string(page);
+            var rget = new Http(find);
+            
+            rget.onData = function (data:String) {  
+                var jlist:GFile = Json.parse(data); 
+                var blacklist:Array<String> = R34.blackList;
+
+                var r = Math.ceil(Std.random(jlist.count));
+                var choose = jlist.posts[r];
+                        
+                if (choose != null) { 
+                    var taglist:Array<String> = choose.tags.split(" ");
+                        
+                    var finding = true;
+                    while (finding == true)  {
+                        var fail = false;
+                        for (tag in taglist)
+                        {   
+                            var result:Int = blacklist.indexOf(tag);
+                            if (result >= 0) {
+                                fail = true;
+                                break;
+                            }
+                        } 
+
+                        if (!fail) {
+                            finding = false;
+                            Main.sendMessage(choose.file_url, animeChan);
+                        }
+                        else {
+                            if (++r < jlist.count) {
+                                choose = jlist.posts[r];
+                                taglist = choose.tags.split(" ");
+                            } else {
+                                finding = false;
+                            }
+                        }
+                    }         
+                } 
+            }
+            rget.request();
+        }  
+    }
+    
+    public static function onClose(c:Int) {
+        var j = Json.stringify(R34.blackList);
+        File.saveContent("mlist.txt", j);
+
+        var rs:RSSFile = {tags:rssLink, page:rssPage};
+        var rj = Json.stringify(rs);
+        File.saveContent("rss.txt",rj);
+
+        trace("ya ypal");
+        Sys.exit(228);
+    }
+
+    public static function sendMessage(text:String, channleId:String) {
+        var msg:MessageCreate = {
+            content: text
+        };
+        var end = new Endpoints(bot);
+        end.sendMessage(channleId, msg);
+    }
+
+    public static function onMessage(m:Message) {
+        if (StringTools.startsWith(m.content, pref)) {
+            var words:Array<String> = m.content.split(" ");
+
+            var command = words.shift();
+            command = StringTools.replace(command, pref, "");
+            
+            var c = commandList.indexOf(command);
+            if (c >= 0) {
+                Reflect.callMethod(Main, Reflect.field(Main,commandList[c]),[m, words]);
+            }
+        }
+    }
 
     @Command
     public static function r(m:Message, words:Array<String>) {
@@ -116,12 +222,167 @@ class Main {
             var choose:RFile = jlist[r];  
             if (choose != null) { 
                 var nm = StringTools.replace(choose.file_url, "https://r34-json-api.herokuapp.com/images?url=","");
-                sendMessage('|| $nm ||', m.channel_id.id);    
+                if (m.inGuild())
+                    sendMessage('||$nm ||', m.channel_id.id);   
+                else 
+                    sendMessage(nm, m.channel_id.id);  
             } else {
                 sendMessage("ничего не нашел", m.channel_id.id);
             }
         }
         rget.request();
+    }
+
+    @Command 
+    public static function rlist(m:Message) {
+        sendMessage(R34.blackList.toString(), m.channel_id.id);
+    }
+
+    
+    @Command 
+    public static function g(m:Message, words:Array<String>) {
+
+        var find = "https://gelbooru-xsd8bjco8ukx.runkit.sh/posts?tags=";
+        for(w in words)
+            find += "+" + w;
+
+        var rget = new Http(find);
+        rget.onData = function (data:String) {  
+
+            var jlist:GFile = Json.parse(data); 
+            var blacklist:Array<String> = R34.blackList;
+
+            var r = Math.ceil(Std.random(jlist.count));
+            var choose = jlist.posts[r];
+                    
+            if (choose != null) { 
+                var taglist:Array<String> = choose.tags.split(" ");
+                    
+                var finding = true;
+                while (finding == true)  {
+                    var fail = false;
+                    for (tag in taglist)
+                    {   
+                        var result:Int = blacklist.indexOf(tag);
+                        if (result >= 0) {
+                            fail = true;
+                            break;
+                        }
+                    } 
+
+                    if (!fail) {
+                        finding = false;
+                        Main.sendMessage(choose.file_url, m.channel_id.id);
+                    }
+                    else {
+                        if (++r < jlist.count) {
+                            choose = jlist.posts[r];
+                            taglist = choose.tags.split(" ");
+                        } else {
+                            finding = false;
+                            Main.sendMessage("мне нельзя такое показывать", m.channel_id.id);
+                        }
+                    }
+                }         
+            } else {
+                Main.sendMessage("ничего не нашел", m.channel_id.id);
+            }
+        }
+
+        rget.request();
+    }
+    
+    @Command 
+    public static function ga(m:Message, words:Array<String>) {
+
+        var find = "https://gelbooru-xsd8bjco8ukx.runkit.sh/posts?tags=";
+        for(w in words)
+            find += "+" + w;
+
+        var rget = new Http(find);
+        rget.onData = function (data:String) {  
+
+            var jlist:GFile = Json.parse(data); 
+            var blacklist:Array<String> = R34.blackList;
+
+            var r = Math.ceil(Std.random(jlist.count));
+            var choose = jlist.posts[r];
+                    
+            if (choose != null) { 
+                if (m.inGuild())
+                    Main.sendMessage("||"+choose.file_url+" ||", m.channel_id.id);
+                else 
+                    Main.sendMessage(choose.file_url, m.channel_id.id);
+            } else {
+                Main.sendMessage("ничего не нашел", m.channel_id.id);
+            }
+        }
+
+        rget.request();
+    }
+
+    @Command
+    public static function info(m:Message) {
+        var aut:EmbedAuthor = {
+            name: "Fatalny_podsos",
+            icon_url: bot.user.avatarUrl,
+        }
+
+        var f1:EmbedField = {
+            name: "r теги через пробел",
+            value: "Показывает случайную пикчу с заданными тегами c rule34.xxx. имена персонажей указывать одним тегом через нижний прочерк onigawara_rin, нежелательные варианты отсеиваются черным списком",        
+        };
+        
+        var f2:EmbedField = {
+            name: "ra теги через пробел",
+            value: "аналогично команде r, но без черного списка, однако результат будет под спойлером. в лс спойлеров не будет",      
+        };
+
+        var f11:EmbedField = {
+            name: "g теги через пробел",
+            value: "Показывает случайную пикчу с заданными тегами c gelbooru.com. имена персонажей указывать одним тегом через нижний прочерк onigawara_rin, нежелательные варианты отсеиваются черным списком",        
+        };
+        
+        var f21:EmbedField = {
+            name: "ga теги через пробел",
+            value: "аналогично команде g, но без черного списка, однако результат будет под спойлером. в лс спойлеров не будет",      
+        };
+        
+        var f3:EmbedField = {
+            name: "rlist",
+            value: "черный список тегов"
+        };
+
+        var embed:Embed = {
+            author: aut,
+            fields: [f1,f2,f11,f21,f3],
+            color: 0,
+        };
+    
+        var msg:MessageCreate = {
+            embed: embed,
+        };
+        var end = new Endpoints(bot);
+        end.sendMessage(m.channel_id.id, msg);
+    }
+   
+
+    @Command 
+    public static function status(m:Message, words:Array<String>) {
+        if (m.getMember().user.id.id != "371690693233737740")
+            return;
+        
+        var a:Activity = {
+            name: words.join(" "),
+            type: 0,
+        };
+        var s:Status = {
+            game: a,
+            afk: false,
+            status: "online",
+        };
+
+        bot.setStatus(s);
     }
 
     @Command
@@ -150,92 +411,24 @@ class Main {
         }
     }
 
-    @Command 
-    public static function rlist(m:Message) {
-        sendMessage(R34.blackList.toString(), m.channel_id.id);
+
+    @Command
+    public static function rss_link(m:Message, words:Array<String>) {
+        if (m.getMember().user.id.id != "371690693233737740")
+            return;
+        rssLink = words.shift();
+        sendMessage('автопост по ссылке `$rssLink`', m.channel_id.id);
     }
-
-    public static function onMessage(m:Message) {
-        if (StringTools.startsWith(m.content, pref)) {
-            var words:Array<String> = m.content.split(" ");
-
-            var command = words.shift();
-            command = StringTools.replace(command, pref, "");
-            
-            var c = commandList.indexOf(command);
-            if (c >= 0) {
-                Reflect.callMethod(Main, Reflect.field(Main,commandList[c]),[m, words]);
-            }
-        }
+    @Command
+    public static function rss_page(m:Message, words:Array<String>) {
+        if (m.getMember().user.id.id != "371690693233737740")
+            return;
+        rssPage = Std.parseInt(words.shift());
+        sendMessage('автопост из `$rssPage` страниц', m.channel_id.id);
     }
-
-    public static function onReady() {
-        trace('invite link: ${bot.getInviteLink()}');
-
-
-        var timer:haxe.Timer = new haxe.Timer(60 * 1000 * 10);
-        timer.run = function() 
-        {   
-            var animeChan = "638490021678284820";
-            var page = Std.random(5);
-            var find = "https://r34-json-api.herokuapp.com/posts?tags=cute+female+human&pid="+Std.string(page);
-            var rget = new Http(find);
-            rget.onData = function (data:String)
-            {
-                var jlist:Array<Dynamic> = Json.parse(data);
-                var blacklist:Array<String> = R34.blackList;
-                blacklist.push("girly");
-
-                var r = Math.ceil(Std.random(jlist.length));
-                var choose:RFile = jlist[r];
-                if (choose != null) { 
-                    var taglist:Array<String> = choose.tags;
-                
-                    var finding = true;
-                    while (finding == true)  {
-                        var fail = false;
-                        for (tag in taglist)
-                        {   
-                            var result:Int = blacklist.indexOf(tag);
-                            if (result >= 0) {
-                                fail = true;
-                                break;
-                            }
-                        } 
-                        if (!fail) {
-                            finding = false;
-                            var nm = StringTools.replace(choose.file_url, "https://r34-json-api.herokuapp.com/images?url=","");
-                            sendMessage(nm, animeChan);
-                        }
-                        else {
-                            if (++r < jlist.length) {
-                                choose = jlist[r];
-                                taglist = choose.tags;
-                            } else {
-                                finding = false;
-                            }
-                        }
-                    }  
-                } 
-            }
-            rget.request();
-        }  
-    }
-    
-    public static function onClose(c:Int) {
-        var j = haxe.Json.stringify(R34.blackList);
-        File.saveContent("mlist.txt", j);
-        trace("server ypal");
-
-        Sys.exit(228);
-    }
-
-    public static function sendMessage(text:String, channleId:String) {
-        var msg:MessageCreate = {
-            content: text
-        };
-        var end = new Endpoints(bot);
-        end.sendMessage(channleId, msg);
+    @Command
+    public static function rss_info(m:Message) {
+        sendMessage('автопост по ссылке `$rssLink$rssPage`', m.channel_id.id);
     }
 
 }
@@ -247,7 +440,17 @@ typedef RFile = {
     var tags:Array<String>;
 }
 
+typedef GFile = {
+    var total:Int;
+    var count:Int;
+    var page:Int;
+    var posts:Array<{score:String, file_url:String, rating:String, tags:String, id:String}>;
+}
 
+typedef RSSFile = {
+    var page:Int;
+    var tags:String;
+}
 
 class R34 {
     public static var blackList:Array<String> = [
@@ -276,6 +479,6 @@ class R34 {
         "chubby",
         "feral",
         "anthro",
-        "girly"
+        "girly",
     ];
 }
